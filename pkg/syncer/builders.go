0c4879e09ae5b2c2eab2268bf8713ce94d8148d5
David Festal <dfestal@redhat.com>
2021-11-03 10:43:32 +0100

    Rename the 'private' label to 'ready-to-sync'
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/syncer/builders.go
+++ pkg/syncer/builders.go
@@ -7,17 +7,17 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-type SyncerBuilder func(upstream, downstream *rest.Config, resources sets.String, numSyncerThreads int)  (*Syncer, error)
+type SyncerBuilder func(upstream, downstream *rest.Config, resources sets.String, numSyncerThreads int) (*Syncer, error)
 
 func BuildSyncerToPhysicalLocation(locationName string) SyncerBuilder {
-	assignedToTheLocation := labels.SelectorFromSet(map[string]string {"kcp.dev/cluster": locationName})
-	return func(upstream, downstream *rest.Config, resources sets.String, numSyncerThreads int)  (*Syncer, error) {
+	assignedToTheLocation := labels.SelectorFromSet(map[string]string{"kcp.dev/cluster": locationName})
+	return func(upstream, downstream *rest.Config, resources sets.String, numSyncerThreads int) (*Syncer, error) {
 		return StartSyncer(upstream, downstream, assignedToTheLocation, NewIdentitySyncing(nil), resources, numSyncerThreads)
 	}
 }
 
 func BuildCustomSyncerToPrivateLogicalCluster(transformerClass string, syncing Syncing) (SyncerBuilder, error) {
-	syncing.LabelsToAdd()["kcp.dev/private"] = ""
+	syncing.LabelsToAdd()["kcp.dev/ready-for-sync"] = ""
 
 	assignedToALocation, err := labels.NewRequirement("kcp.dev/cluster", selection.Exists, []string{})
 	if err != nil {
@@ -28,15 +28,15 @@ func BuildCustomSyncerToPrivateLogicalCluster(transformerClass string, syncing S
 		tranformerReq, err = labels.NewRequirement("kcp.dev/transformer", selection.DoesNotExist, []string{})
 		if err != nil {
 			return nil, err
-		}	
+		}
 	} else {
-		tranformerReq, err = labels.NewRequirement("kcp.dev/transformer", selection.Equals, []string{ transformerClass })
+		tranformerReq, err = labels.NewRequirement("kcp.dev/transformer", selection.Equals, []string{transformerClass})
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	return func(upstream, downstream *rest.Config, resources sets.String, numSyncerThreads int)  (*Syncer, error) {
+	return func(upstream, downstream *rest.Config, resources sets.String, numSyncerThreads int) (*Syncer, error) {
 		return StartSyncer(upstream, downstream, labels.NewSelector().Add(*assignedToALocation, *tranformerReq), syncing, resources, numSyncerThreads)
 	}, nil
 }

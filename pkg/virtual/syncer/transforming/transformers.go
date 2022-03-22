4e065411cbb3edfa574ff9acc51b07e19572f07b
David Festal <dfestal@redhat.com>
2022-03-22 12:07:08 +0100

    Also allow transforming request result
    Signed-off-by: David Festal <dfestal@redhat.com>

--- pkg/virtual/syncer/transforming/transformers.go
+++ pkg/virtual/syncer/transforming/transformers.go
@@ -19,9 +19,12 @@ package transforming
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/watch"
 )
 
 type UpdateSelectorsFunc func(ctx context.Context, labelSelector labels.Selector, fieldSelector fields.Selector) (labels.Selector, fields.Selector, error)
@@ -66,4 +69,112 @@ func TransformsSelectors(updateFunc UpdateSelectorsFunc) Transformer {
 			return ctx, opts, nil
 		},
 	}
+}
+
+type ResourceTransformer func(ctx context.Context, resource *unstructured.Unstructured, subresources ...string) (transformed *unstructured.Unstructured, err error)
+
+func TransformsResource(before, after ResourceTransformer) Transformer {
+	t := Transformer{}
+
+	if before != nil {
+		t.BeforeCreate = func(ctx context.Context, obj *unstructured.Unstructured, options metav1.CreateOptions, subresources ...string) (context.Context, *unstructured.Unstructured, metav1.CreateOptions, []string, error) {
+			if transformedResource, err := before(ctx, obj, subresources...); err != nil {
+				return ctx, obj, options, subresources, err
+			} else {
+				return ctx, transformedResource, options, subresources, nil
+			}
+		}
+		t.BeforeUpdate = func(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions, subresources ...string) (context.Context, *unstructured.Unstructured, metav1.UpdateOptions, []string, error) {
+			if transformedResource, err := before(ctx, obj, subresources...); err != nil {
+				return ctx, obj, options, subresources, err
+			} else {
+				return ctx, transformedResource, options, subresources, nil
+			}
+		}
+	}
+	if after != nil {
+		t.AfterCreate = func(ctx context.Context, obj *unstructured.Unstructured, options metav1.CreateOptions, subresources []string, result *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+			if transformedResource, err := after(ctx, obj, subresources...); err != nil {
+				return obj, err
+			} else {
+				return transformedResource, nil
+			}
+		}
+		t.AfterUpdate = func(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions, subresources []string, result *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+			if transformedResource, err := after(ctx, obj, subresources...); err != nil {
+				return obj, err
+			} else {
+				return transformedResource, nil
+			}
+		}
+		// TODO: implement the delete, but for now we don't need it
+		t.AfterGet = func(ctx context.Context, name string, options metav1.GetOptions, subresources []string, result *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+			if transformedResource, err := after(ctx, result, subresources...); err != nil {
+				return nil, err
+			} else {
+				return transformedResource, nil
+			}
+		}
+		t.AfterList = func(ctx context.Context, opts metav1.ListOptions, result *unstructured.UnstructuredList) (*unstructured.UnstructuredList, error) {
+			transformedResult := result.DeepCopy()
+			transformedResult.Items = []unstructured.Unstructured{}
+			for _, item := range result.Items {
+				item := item
+				if transformed, err := after(ctx, &item); err != nil {
+					if errors.IsNotFound(err) {
+						continue
+					}
+				} else {
+					transformedResult.Items = append(transformedResult.Items, *transformed)
+				}
+			}
+			return transformedResult, nil
+		}
+		t.AfterWatch = func(ctx context.Context, opts metav1.ListOptions, result watch.Interface) (watch.Interface, error) {
+			transformingWatcher := NewTransformingWatcher(result, func(event watch.Event) (transformed watch.Event, skipped bool) {
+				transformed = event
+				switch event.Type {
+				case watch.Added, watch.Modified, watch.Deleted:
+					if resource, isUnstructured := event.Object.(*unstructured.Unstructured); !isUnstructured {
+						transformed.Type = watch.Error
+						transformed.Object = &metav1.Status{
+							Status:  "Failure",
+							Reason:  metav1.StatusReasonUnknown,
+							Message: "Watch expected a resource of type *unstructured.Unstructured",
+							Code:    500,
+						}
+					} else if transformedResource, err := after(ctx, resource); err != nil {
+						if errors.IsNotFound(err) {
+							skipped = true
+						} else {
+							transformed.Type = watch.Error
+							transformed.Object = &metav1.Status{
+								Status:  "Failure",
+								Reason:  metav1.StatusReasonUnknown,
+								Message: "Watch transformation failed",
+								Code:    500,
+								Details: &metav1.StatusDetails{
+									Name:  resource.GetName(),
+									Group: resource.GroupVersionKind().Group,
+									Kind:  resource.GroupVersionKind().Kind,
+									Causes: []metav1.StatusCause{
+										{
+											Type:    metav1.CauseTypeUnexpectedServerResponse,
+											Message: err.Error(),
+										},
+									},
+								},
+							}
+						}
+					} else {
+						transformed.Object = transformedResource
+					}
+				}
+				return transformed, skipped
+			})
+			return transformingWatcher, nil
+		}
+	}
+
+	return t
 }
--- pkg/virtual/syncer/transforming/types.go
+++ pkg/virtual/syncer/transforming/types.go
@@ -32,13 +32,23 @@ type TypedTransformers map[schema.GroupVersionResource]Transformers
 type Transformers []Transformer
 
 type Transformer struct {
-	BeforeCreate     func(ctx context.Context, obj *unstructured.Unstructured, options metav1.CreateOptions, subresources ...string) (context.Context, *unstructured.Unstructured, metav1.CreateOptions, []string, error)
-	BeforeUpdate     func(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions, subresources ...string) (context.Context, *unstructured.Unstructured, metav1.UpdateOptions, []string, error)
-	BeforeDelete     func(ctx context.Context, name string, options metav1.DeleteOptions, subresources ...string) (context.Context, string, metav1.DeleteOptions, []string, error)
-	DeleteCollection func(ctx context.Context, options metav1.DeleteOptions, listOptions metav1.ListOptions) (context.Context, metav1.DeleteOptions, metav1.ListOptions, error)
-	BeforeGet        func(ctx context.Context, name string, options metav1.GetOptions, subresources ...string) (context.Context, string, metav1.GetOptions, []string, error)
-	BeforeList       func(ctx context.Context, opts metav1.ListOptions) (context.Context, metav1.ListOptions, error)
-	BeforeWatch      func(ctx context.Context, opts metav1.ListOptions) (context.Context, metav1.ListOptions, error)
+	BeforeCreate func(ctx context.Context, obj *unstructured.Unstructured, options metav1.CreateOptions, subresources ...string) (context.Context, *unstructured.Unstructured, metav1.CreateOptions, []string, error)
+	AfterCreate  func(ctx context.Context, obj *unstructured.Unstructured, options metav1.CreateOptions, subresources []string, result *unstructured.Unstructured) (*unstructured.Unstructured, error)
+
+	BeforeUpdate func(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions, subresources ...string) (context.Context, *unstructured.Unstructured, metav1.UpdateOptions, []string, error)
+	AfterUpdate  func(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions, subresources []string, result *unstructured.Unstructured) (*unstructured.Unstructured, error)
+
+	BeforeDelete           func(ctx context.Context, name string, options metav1.DeleteOptions, subresources ...string) (context.Context, string, metav1.DeleteOptions, []string, error)
+	BeforeDeleteCollection func(ctx context.Context, options metav1.DeleteOptions, listOptions metav1.ListOptions) (context.Context, metav1.DeleteOptions, metav1.ListOptions, error)
+
+	BeforeGet func(ctx context.Context, name string, options metav1.GetOptions, subresources ...string) (context.Context, string, metav1.GetOptions, []string, error)
+	AfterGet  func(ctx context.Context, name string, options metav1.GetOptions, subresources []string, result *unstructured.Unstructured) (*unstructured.Unstructured, error)
+
+	BeforeList func(ctx context.Context, opts metav1.ListOptions) (context.Context, metav1.ListOptions, error)
+	AfterList  func(ctx context.Context, opts metav1.ListOptions, result *unstructured.UnstructuredList) (*unstructured.UnstructuredList, error)
+
+	BeforeWatch func(ctx context.Context, opts metav1.ListOptions) (context.Context, metav1.ListOptions, error)
+	AfterWatch  func(ctx context.Context, opts metav1.ListOptions, result watch.Interface) (watch.Interface, error)
 }
 
 type TransformingClient struct {
@@ -47,7 +57,32 @@ type TransformingClient struct {
 }
 
 func (tc *TransformingClient) Create(ctx context.Context, obj *unstructured.Unstructured, options metav1.CreateOptions, subresources ...string) (*unstructured.Unstructured, error) {
-	return nil, errors.New("not implemented")
+	var err error
+	for _, transformer := range tc.Transformations {
+		action := transformer.BeforeCreate
+		if action == nil {
+			continue
+		}
+		ctx, obj, options, subresources, err = action(ctx, obj, options, subresources...)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result, err := tc.Client.Create(ctx, obj, options, subresources...)
+	if err != nil {
+		return result, err
+	}
+	for _, transformer := range tc.Transformations {
+		action := transformer.AfterCreate
+		if action == nil {
+			continue
+		}
+		result, err = action(ctx, obj, options, subresources, result)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
 }
 func (tc *TransformingClient) Update(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions, subresources ...string) (*unstructured.Unstructured, error) {
 	var err error
@@ -61,7 +96,21 @@ func (tc *TransformingClient) Update(ctx context.Context, obj *unstructured.Unst
 			return nil, err
 		}
 	}
-	return tc.Client.Update(ctx, obj, options, subresources...)
+	result, err := tc.Client.Update(ctx, obj, options, subresources...)
+	if err != nil {
+		return result, err
+	}
+	for _, transformer := range tc.Transformations {
+		action := transformer.AfterUpdate
+		if action == nil {
+			continue
+		}
+		result, err = action(ctx, obj, options, subresources, result)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
 }
 func (tc *TransformingClient) UpdateStatus(ctx context.Context, obj *unstructured.Unstructured, options metav1.UpdateOptions) (*unstructured.Unstructured, error) {
 	return nil, errors.New("not implemented")
@@ -73,7 +122,32 @@ func (tc *TransformingClient) DeleteCollection(ctx context.Context, options meta
 	return errors.New("not implemented")
 }
 func (tc *TransformingClient) Get(ctx context.Context, name string, options metav1.GetOptions, subresources ...string) (*unstructured.Unstructured, error) {
-	return nil, errors.New("not implemented")
+	var err error
+	for _, transformer := range tc.Transformations {
+		action := transformer.BeforeGet
+		if action == nil {
+			continue
+		}
+		ctx, name, options, subresources, err = action(ctx, name, options, subresources...)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result, err := tc.Client.Get(ctx, name, options, subresources...)
+	if err != nil {
+		return result, err
+	}
+	for _, transformer := range tc.Transformations {
+		action := transformer.AfterGet
+		if action == nil {
+			continue
+		}
+		result, err = action(ctx, name, options, subresources, result)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
 }
 func (tc *TransformingClient) List(ctx context.Context, opts metav1.ListOptions) (*unstructured.UnstructuredList, error) {
 	var err error
@@ -87,19 +161,95 @@ func (tc *TransformingClient) List(ctx context.Context, opts metav1.ListOptions)
 			return nil, err
 		}
 	}
-	return tc.Client.List(ctx, opts)
+	result, err := tc.Client.List(ctx, opts)
+	if err != nil {
+		return result, err
+	}
+	for _, transformer := range tc.Transformations {
+		action := transformer.AfterList
+		if action == nil {
+			continue
+		}
+		result, err = action(ctx, opts, result)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
 }
 func (tc *TransformingClient) Watch(ctx context.Context, opts metav1.ListOptions) (watch.Interface, error) {
 	var err error
 	for _, transformer := range tc.Transformations {
 		action := transformer.BeforeWatch
+		if action == nil {
+			continue
+		}
 		ctx, opts, err = action(ctx, opts)
 		if err != nil {
 			return nil, err
 		}
 	}
-	return tc.Client.Watch(ctx, opts)
+	result, err := tc.Client.Watch(ctx, opts)
+	if err != nil {
+		return result, err
+	}
+	for _, transformer := range tc.Transformations {
+		action := transformer.AfterWatch
+		if action == nil {
+			continue
+		}
+		result, err = action(ctx, opts, result)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
 }
+
 func (tc *TransformingClient) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, options metav1.PatchOptions, subresources ...string) (*unstructured.Unstructured, error) {
 	return nil, errors.New("not implemented")
+}
+
+type TransformingWatcher struct {
+	source           watch.Interface
+	transformedCh    chan watch.Event
+	eventTransformer EventTransformer
+}
+
+type EventTransformer func(event watch.Event) (transformed watch.Event, skipped bool)
+
+func NewTransformingWatcher(watcher watch.Interface, eventTransformer EventTransformer) *TransformingWatcher {
+	tw := &TransformingWatcher{
+		source:           watcher,
+		transformedCh:    make(chan watch.Event),
+		eventTransformer: eventTransformer,
+	}
+	tw.start()
+	return tw
+}
+
+func (w *TransformingWatcher) start() {
+	go func() {
+		for {
+			if evt, more := <-w.source.ResultChan(); more {
+				transformedEvent, skipped := w.eventTransformer(evt)
+				if !skipped {
+					w.transformedCh <- transformedEvent
+				}
+			} else {
+				close(w.transformedCh)
+				return
+			}
+		}
+	}()
+}
+
+// Stop implements Interface
+func (w *TransformingWatcher) Stop() {
+	w.source.Stop()
+}
+
+// ResultChan implements Interface
+func (w *TransformingWatcher) ResultChan() <-chan watch.Event {
+	return w.transformedCh
 }
